530ec3eab88b96db6eda5e0878ee4124d2e46f9c
Vincent Goudket <vincent.goudket@X9CQ02HK96.io-internal.net>
2023-05-26 15:44:31 +0200

    First commit

---
+++ episode-03/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Demo</title>
+    </head>
+    <body>
+        <h1>
+            <?php 
+                echo "Hello at the other side";
+                // print "Hello at the other side";
+            ?>
+        </h1>
+        <p>
+            <?php 
+                echo "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eos ipsam asperiores, nobis minus deleniti sed id dolorum officia tempore commodi porro animi itaque molestias nemo quis repellendus labore est!";
+            ?>
+        </p>
+        <a href="#">
+            <?php 
+                echo "This is a link";
+            ?>
+        </a>
+        <button>
+            <?php 
+                echo "This is a button";
+            ?>
+        </button>
+    </body>
+</html>
